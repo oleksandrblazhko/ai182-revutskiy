475b30c4cedd2fe988c5a37c07a2f7738c5b95ee
rrevutskiy <rrevutskiy@reaktivate.com>
2022-12-31 22:03:41 +0200

    10.0.0

---
+++ laboratory-work-10/get_data.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION get_data(p_username VARCHAR, p_token VARCHAR, human_name VARCHAR)
+RETURNS TABLE (p_id INTEGER, name VARCHAR, bd DATE) AS $$
+DECLARE
+	str VARCHAR;
+BEGIN
+    CALL sso_control(p_username, p_token);
+	str := 'SELECT p_id, name, bd FROM human WHERE name = $1';
+	RAISE NOTICE 'Query=%', str;
+	RETURN QUERY EXECUTE str USING human_name;
+END;
+$$ LANGUAGE plpgsql;
